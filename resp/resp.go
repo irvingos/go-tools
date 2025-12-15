0b2240d27c8bdc3ef21e95b3842d90d9972f8f2c
wei.zhang04 <wei.zhang04@narwal.com>
2025-12-15 10:54:58 +0800

    feat: 补充 resp

---
+++ resp/resp.go
@@ -0,0 +1,74 @@
+package resp
+
+import (
+	"errors"
+	"io"
+	"net/http"
+
+	"github.com/irvingos/go-tools/errorx"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+var (
+	errEmptyParam    = errorx.NewError(1004100, "empty request param", http.StatusBadRequest)
+	errValidateParam = errorx.NewError(1004101, "error validate param", http.StatusBadRequest)
+	errResolveParam  = errorx.NewError(1004102, "error resolve param", http.StatusBadRequest)
+)
+
+type Response struct {
+	Code    int    `json:"code"`
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Detail  string `json:"detail,omitempty"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func OK(g *gin.Context, data any) {
+	g.JSON(http.StatusOK, Response{
+		Success: true,
+		Message: "success",
+		Data:    data,
+	})
+}
+
+func ErrorParam(g *gin.Context, err error) {
+	if errors.Is(err, io.EOF) {
+		g.AbortWithStatusJSON(errEmptyParam.HTTPStatus(), Response{
+			Code:    errEmptyParam.Code(),
+			Message: errEmptyParam.Message(),
+			Detail:  err.Error(),
+		})
+		return
+	}
+	var ve validator.ValidationErrors
+	if ok := errors.As(err, &ve); ok {
+		g.AbortWithStatusJSON(errValidateParam.HTTPStatus(), Response{
+			Code:    errValidateParam.Code(),
+			Message: errValidateParam.Message(),
+			Detail:  err.Error(),
+		})
+		return
+	}
+	g.AbortWithStatusJSON(errResolveParam.HTTPStatus(), Response{
+		Code:    errResolveParam.Code(),
+		Message: errResolveParam.Message(),
+		Detail:  err.Error(),
+	})
+}
+
+func Error(g *gin.Context, err error) {
+	if apiErr, ok := err.(errorx.Error); ok {
+		g.AbortWithStatusJSON(apiErr.HTTPStatus(), Response{
+			Code:    apiErr.Code(),
+			Message: apiErr.Message(),
+		})
+		return
+	}
+
+	g.AbortWithStatusJSON(http.StatusInternalServerError, Response{
+		Code:    errorx.ErrInternal.Code(),
+		Message: errorx.ErrInternal.Message(),
+	})
+}
