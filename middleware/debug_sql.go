7088a3f8f0d3d6020b1fd1e750a92190915d552b
今晚打老虎 <1872101334@qq.com>
2025-12-21 16:41:36 +0800

    feat: debug sql middleware

--- middleware/debug_sql.go
+++ middleware/debug_sql.go
@@ -1,13 +1,15 @@
 package middleware
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/irvingos/go-tools/logx"
 )
 
 func DebugSQLMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if c.GetHeader("X-Debug-SQL") == "true" {
+		if debugSQL, err := strconv.ParseBool(c.GetHeader("X-Debug-SQL")); err != nil && debugSQL {
 			ctx := logx.WithTraceSQL(c.Request.Context())
 			c.Request = c.Request.WithContext(ctx)
 		}
