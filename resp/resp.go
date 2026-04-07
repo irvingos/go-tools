93edcb0fa0ba0e3660eb04e51d49e9bae1d4fa27
今晚打老虎 <1872101334@qq.com>
2026-04-07 18:35:13 +0800

    fix: ensure error handling updates the context in gin.Request for consistent status code responses

--- resp/resp.go
+++ resp/resp.go
@@ -38,7 +38,7 @@ func ErrorParam(g *gin.Context, err error) {
 			Message: errEmptyParam.Message(),
 			Detail:  err.Error(),
 		})
-		WithCode(g.Request.Context(), errEmptyParam.Code())
+		g.Request = g.Request.WithContext(WithCode(g.Request.Context(), errEmptyParam.Code()))
 		return
 	}
 	var ve validator.ValidationErrors
@@ -48,7 +48,7 @@ func ErrorParam(g *gin.Context, err error) {
 			Message: errValidateParam.Message(),
 			Detail:  err.Error(),
 		})
-		WithCode(g.Request.Context(), errValidateParam.Code())
+		g.Request = g.Request.WithContext(WithCode(g.Request.Context(), errValidateParam.Code()))
 		return
 	}
 	g.AbortWithStatusJSON(http.StatusOK, Response{
@@ -56,7 +56,7 @@ func ErrorParam(g *gin.Context, err error) {
 		Message: errResolveParam.Message(),
 		Detail:  err.Error(),
 	})
-	WithCode(g.Request.Context(), errResolveParam.Code())
+	g.Request = g.Request.WithContext(WithCode(g.Request.Context(), errResolveParam.Code()))
 }
 
 func Error(g *gin.Context, err error) {
@@ -65,7 +65,7 @@ func Error(g *gin.Context, err error) {
 			Code:    apiErr.Code(),
 			Message: apiErr.Message(),
 		})
-		WithCode(g.Request.Context(), apiErr.Code())
+		g.Request = g.Request.WithContext(WithCode(g.Request.Context(), apiErr.Code()))
 		return
 	}
 
@@ -74,5 +74,5 @@ func Error(g *gin.Context, err error) {
 		Message: errorx.ErrInternal.Message(),
 		Detail:  err.Error(),
 	})
-	WithCode(g.Request.Context(), errorx.ErrInternal.Code())
+	g.Request = g.Request.WithContext(WithCode(g.Request.Context(), errorx.ErrInternal.Code()))
 }
