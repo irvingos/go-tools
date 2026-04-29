be2bb3db289b9d95d7c0f3296c3f3581ca390cba
今晚打老虎 <1872101334@qq.com>
2026-04-29 18:24:50 +0800

    refactor: rename middleware functions for clarity and consistency

--- middleware/debug_sql.go
+++ middleware/debug_sql.go
@@ -7,7 +7,7 @@ import (
 	"github.com/irvingos/go-tools/logx"
 )
 
-func DebugSQLMiddleware() gin.HandlerFunc {
+func DebugSQL() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if debugSQL, err := strconv.ParseBool(c.GetHeader("X-Debug-SQL")); err == nil && debugSQL {
 			ctx := logx.WithTraceSQL(c.Request.Context())
--- middleware/recovery.go
+++ middleware/recovery.go
@@ -21,7 +21,7 @@ import (
 
 const maxStackBytes = 64 << 10 // 64KB
 
-func RecoveryMiddleware(hideHeaders []string, responder *resp.Responder) gin.HandlerFunc {
+func Recovery(hideHeaders []string, responder *resp.Responder) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ecw := &errCapturingWriter{ResponseWriter: ctx.Writer}
 		ctx.Writer = ecw
