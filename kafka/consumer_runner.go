02c3f512095f8f23d5329791c534b41b82a10729
今晚打老虎 <1872101334@qq.com>
2026-06-18 17:55:34 +0800

    refactor: add name field to ConsumerRunner and update constructor for improved identification

--- kafka/consumer_runner.go
+++ kafka/consumer_runner.go
@@ -3,14 +3,19 @@ package kafka
 import "context"
 
 type ConsumerRunner struct {
+	name     string
 	consumer Consumer
 	handler  MessageHandler
 }
 
-func NewConsumerRunner(consumer Consumer, handler MessageHandler) *ConsumerRunner {
-	return &ConsumerRunner{consumer: consumer, handler: handler}
+func NewConsumerRunner(name string, consumer Consumer, handler MessageHandler) *ConsumerRunner {
+	return &ConsumerRunner{name: name, consumer: consumer, handler: handler}
 }
 
 func (r *ConsumerRunner) Run(ctx context.Context) error {
 	return r.consumer.Run(ctx, r.handler)
+}
+
+func (r *ConsumerRunner) Name() string {
+	return r.name
 }
